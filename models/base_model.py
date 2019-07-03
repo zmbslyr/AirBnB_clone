1d2fb93aecf4949d3f2b296ee7b567da7fa782e6
tyedge <tyedge.inc@gmail.com>
2019-07-03 16:29:10 +0000

    Reverting back to Task 3 for checker

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ This module defines the BaseModel class and its attributes """
 
-from models import storage
 from datetime import datetime
 from os import path
 import json
@@ -12,24 +11,11 @@ class BaseModel:
     """ This class is the model defining all the common attributes for other\
     classes """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """ This function initializes a new instance of the BaseModel class """
-        if args is not None:
-            pass
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key == '__class__':
-                    value = BaseModel
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ This function print a special string for instances of the BaseModel\
@@ -40,12 +26,12 @@ class BaseModel:
         """ This function updates and saves the 'updated_at' attribute for \
         instances of the BaseModel class """
         self.updated_at = datetime.now()
-        storage.save()
+        return self.updated_at
 
     def to_dict(self):
         """ This function returns a dictionary representation of a BaseModel\
         instance """
         self.__dict__['updated_at'] = '%s' % (self.updated_at.isoformat())
         self.__dict__['created_at'] = '%s' % (self.created_at.isoformat())
-        self.__dict__['__class__'] = 'BaseModel'
+        self.__dict__['__class__'] = "BaseModel"
         return self.__dict__
