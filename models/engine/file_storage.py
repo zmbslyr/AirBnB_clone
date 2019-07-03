5dd6f42198fc16f3cdb7eb3e0bd98f2b55fb21a6
zmbslyr <mhedgeland@gmail.com>
2019-07-03 22:03:34 +0000

    [GLOBAL][storage] Importing classes into save method && push origin master

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ This module defines the FileStorage class and its attributes """
 
-from models.base_model import BaseModel
 from os import path
 import json
 
@@ -27,6 +26,14 @@ class FileStorage:
     def save(self):
         """ This function serializes __objects to the JSON file
         (path: __file_path) """
+
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
         newd = FileStorage.__objects.copy()
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
             for k, v in newd.items():
